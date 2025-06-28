d961cfebc3f5e8d8b36d61d035883c301fffae25
Nick Deupree <nicholasdeupree@gmail.com>
2025-06-28 16:33:30 -0400

    Add game management functions and Firebase integration
    - Implemented game hosting, updating, searching, and deletion functionalities in gameUtils.tsx.
    - Enhanced hostLogic.tsx with additional Firebase storage capabilities.
    - Updated firebase.ts to include Firestore, Storage, and Auth initialization.

--- src/backend/host/hostLogic.tsx
+++ src/backend/host/hostLogic.tsx
@@ -1,3 +1,4 @@
-import { ref, uploadBytes } from 'firebase/storage';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import {doc, setDoc, getDoc, deleteDoc, Timestamp } from 'firebase/firestore';
-import { storage, db, auth } from 'firebase'
+import { storage, db } from '../../lib/firebase'
+
